2ad850fa42f29e2c71c3553022f10c235180002d
tticc <18883394480@163.com>
2020-04-04 17:29:17 +0800

    react init

---
+++ src/components/ShoppingList/ShoppingList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// 如果需要导出的class，加上 export
+export class ShoppingList extends React.Component{
+    render(){
+        return (<div className="shopping-list">
+            <h1>Shopping list for {this.props.name}</h1>
+            <ul>
+                <li>Instji</li>
+                <li>chicken</li>
+                <li>beef</li>
+            </ul>
+        </div>);
+    }
+}
+export default ShoppingList;
